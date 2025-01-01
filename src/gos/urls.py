3d10aa9e3c1e71376c671017d4e8ee99a6db4d21
Mic <mdaizovi@gmail.com>
2025-01-01 16:58:10 +0700

    basic terms of service stuf

--- src/gos/urls.py
+++ src/gos/urls.py
@@ -1,9 +1,22 @@
+import os
 from django.contrib import admin
-from django.urls import include, path
-from django.urls import re_path
+from django.conf import settings
+from django.urls import path, include, re_path
+from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api-auth/", include("rest_framework.urls")),
-    re_path(r"^auth/", include("drf_social_oauth2.urls", namespace="drf")),
+
+    path('terms-of-service/', TemplateView.as_view(template_name="tos.html"), name='terms-of-service'),
+    path('privacy-policy/', TemplateView.as_view(template_name="privacy_policy_app.html"), name='privacy'),
+    path('support/', TemplateView.as_view(template_name="support.html"), name='support'),
+
 ]
+
+if settings.DEBUG==True:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+admin.site.site_header = "Game of Skate Admin Panel"
