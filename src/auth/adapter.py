dc1ed1aa53537ad37bf3e9ee5f6df5df71f3ad94
Mic <mdaizovi@gmail.com>
2025-02-02 23:06:57 +0700

    feat: google login works

--- src/auth/adapter.py
+++ src/auth/adapter.py
@@ -1,87 +1,27 @@
 import json
 from allauth.account.adapter import DefaultAccountAdapter
 from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
-from django.core.exceptions import (
-    ImproperlyConfigured,
-    MultipleObjectsReturned,
-)
 
 class AccountAdapter(DefaultAccountAdapter):
     pass
         
 
-
-# WHY is the client_id alawys none when i go directly to google url?
-
 class SocialAccountAdapter(DefaultSocialAccountAdapter):
 
-    def pre_social_login(self, request, sociallogin):
-        print(f"request.POST {request.POST}")
-        print(f"sociallogin\n{type(sociallogin)}\n{sociallogin}")
-
-
-    # def get_app(self, request, provider, client_id=None):
-    #     print("get app AccountSocialAdapter")
-    #     from allauth.socialaccount.models import SocialApp
-    #     print(f"provider {provider}")
-    #     print(f"client_id {client_id}")
-    #     print(f"request {request}")
-    #     # runs when i go straight to googloe long but not when i use headless properly
-    #     # print(request.body)
-    #     if not client_id:
-    #         print("\n\nwhy no client id? gonna hardcode")
-    #         #client_id = "336313820834-ggh95meblen1bagba1ral3v33ahkgu3n.apps.googleusercontent.com"
-    #         request_body = dict(request.POST)
-    #         client_id = request_body.get("client_id")[0]
-    #         print(f"client_id again {client_id}")
-    #     apps = self.list_apps(request, provider=provider, client_id=client_id)
-    #     print(f"apps {apps}")
-    #     if len(apps) > 1:
-    #         print("more than one")
-    #         visible_apps = [app for app in apps if not app.settings.get("hidden")]
-    #         if len(visible_apps) != 1:
-    #             raise MultipleObjectsReturned
-    #         apps = visible_apps
-    #     elif len(apps) == 0:
-    #         raise SocialApp.DoesNotExist()
-    #     return apps[0]
-    
-
     def get_provider(self, request, provider, client_id=None):
-        """Looks up a `provider`, supporting subproviders by looking up by
-        `provider_id`.
         """
-        from allauth.socialaccount.providers import registry
-        print("get provider AccountSocialAdapter")
-        print(f"provider {provider}")
-        print(f"client_id {client_id}")
-        print(f"request {request}")
-        # print(request.body)
+        Subclassed so that having > 1 client per provider doesn't silently break everything
+        I don't even know if I'll do this, but this was super super confusing to debug
+        So leaving a a reminder.
+        The reason is, no client_id is provided when SocialAccount.get_provider,
+        which happens when new account is created.
+        """
         if not client_id:
-            print("\nno client id in get provider!\n")
+            # check request body for provider, which will be there if this was initiated from
+            # a call to _allauth/app/v1/auth/provider/token
             request_body = json.loads(request.body)
-            # print(f"request_body {request_body}")
             token = request_body.get("token")
             if token:
-                print("token exists!")
                 client_id = token.get("client_id")
-                print(f"client id {client_id }")
 
-        provider_class = registry.get_class(provider)
-        print(f"provider_class {provider_class}")
-        if provider_class is None or provider_class.uses_apps:
-            app = self.get_app(request, provider=provider, client_id=client_id)
-            print(f"app {app}")
-            if not provider_class:
-                # In this case, the `provider` argument passed was a
-                # `provider_id`.
-                provider_class = registry.get_class(app.provider)
-            if not provider_class:
-                raise ImproperlyConfigured(f"unknown provider: {app.provider}")
-            print(f"provider class {provider_class}")
-            return provider_class(request, app=app)
-        elif provider_class:
-            assert not provider_class.uses_apps  # nosec
-            return provider_class(request, app=None)
-        else:
-            raise ImproperlyConfigured(f"unknown provider: {app.provider}")
+        return super().get_provider(request=request, provider=provider, client_id=client_id)
--- src/gos/urls.py
+++ src/gos/urls.py
@@ -1,11 +1,9 @@
-import os
 from django.contrib import admin
 from django.conf import settings
-from django.urls import path, include, re_path
+from django.urls import path, include
 from django.conf.urls.static import static
 from django.views.generic import TemplateView
-from allauth.headless.constants import Client
-from auth.views import login_by_token
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -23,15 +21,12 @@ urlpatterns = [
         "support/", TemplateView.as_view(template_name="support.html"), name="support"
     ),
     path("accounts/", include("allauth.urls")),
-    # Include the API endpoints:
-    path("_allauth/", include("allauth.headless.urls")),
+
+    # path("_allauth/", include("allauth.headless.urls")),
+    # I don't like the headless allauth url structure, renamed here:
+    path("auth/", include("auth.urls")),
     # api-auth only if intend to use the browsable API
     # path("api-auth/", include("rest_framework.urls")),
-    path(
-        "auth/social/login/google",
-        login_by_token,
-        name="google-login",
-    ),
 ]
 
 if settings.DEBUG == True:
